748c3783c83fc5e6f17ad533d98e6bf498245ce3
Mesum <mesumraza09@gmail.com>
2021-07-27 01:30:53 +0500

    show funfact about animal. coding problem solved

---
+++ src/animals.js
@@ -0,0 +1,17 @@
+import dolphinimg from './assets/dolphin.jpg';
+import lobsterimg from './assets/lobster.jpg';
+import starfishimg from './assets/starfish.jpg';
+export const animals = {
+  dolphin: {
+    image: dolphinimg,
+    facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
+  },
+  lobster: {
+    image: lobsterimg,
+    facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
+  },
+  starfish: {
+    image:starfishimg,
+    facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import bg from './assets/ocean.jpg';
+import './styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// import App from './App';
+import { animals } from './animals';
+import reportWebVitals from './reportWebVitals';
+const title= " ";
+//  backgroundimg:url("./assets/ocean.jpg");
+const background=(<img className="background" alt ="ocean"src={bg}/>)
+const showbackground=true;
+const images=[];
+for(const animal in animals) {
+  images.push(
+    <img
+    key={animal}
+    className="animal"
+    alt={animal}
+    src= {animals[animal].image}
+    aria-label= {animal}
+    role= 'button'
+    onClick={displayFact}
+    />
+  )
+}
+function displayFact(e) {
+  const selectedAnimal= e.target.alt;
+  const animalinfo=animals[selectedAnimal];
+  const optionIndex = Math.floor(Math.random() * animalinfo.facts.length);
+  const funFact=animalinfo.facts[optionIndex];
+  document.getElementById('fact').innerHTML= funFact;
+}
+
+const animalFacts = (
+  <div>
+  <h1>{title===" " ? "Click an animal for a fun fact":title} </h1>
+  {showbackground && background}
+  
+  <div className='animals'>
+  {images}
+  </div>
+  <p id='fact'></p>
+</div>)
+
+  ReactDOM.render(animalFacts ,
+    document.getElementById("root"));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
